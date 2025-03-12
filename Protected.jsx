ac87965a7288e80ec7e0690cec071f57ecc010a6
Sachitha <sachunatarajan1234@gmail.com>
2025-03-12 11:16:19 +0530

    updated the image

--- Protected.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const ProtectedRoute = () => {
-  const isAuthenticated = localStorage.getItem("userToken"); // Check if user is logged in
-
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
